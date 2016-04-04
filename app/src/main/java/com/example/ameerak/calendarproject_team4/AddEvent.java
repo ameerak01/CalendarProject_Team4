8fa4c8153892588efa18433c48761382c51a2b1d
ameerak01 <ameera.khan@mavs.uta.edu>
2016-04-03 19:41:57 -0500

    Added 3 buttons. Only the "Share" Button works. Added several text fields for event information. The "Date" Textfield should take input from the datepicker in main.

--- app/src/main/java/com/example/ameerak/calendarproject_team4/AddEvent.java
+++ app/src/main/java/com/example/ameerak/calendarproject_team4/AddEvent.java
@@ -1,13 +1,40 @@
 package com.example.ameerak.calendarproject_team4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddEvent extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat ss = new SimpleDateFormat("dd-MM-yyyy");
+        DatePicker datepick = (DatePicker) this.findViewById(R.id.datePicker_main);;
+        String dateString= ss.format(datepick);
+        TextView editText = (TextView) this.findViewById(R.id.date_editText);
+        // To set the text as the date like MM/DD/YEAR
+        editText.setText(datepick.getMonth() + "/" + datepick.getDayOfMonth() + "/" + datepick.getYear());
+       // editText.setText("Hello");
+
+        // To set the text as the time
+       // editText.setText(datepick.getHours() + ":" + datepick.getMinutes());
     }
+    public void goToShareView(View v)
+    {
+        startActivity(new Intent(this, ShareEvent.class));
+    }
+
+
 }
