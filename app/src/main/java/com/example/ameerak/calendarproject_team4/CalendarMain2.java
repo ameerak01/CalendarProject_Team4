6f60d95ad1ebcc8b21627b20d2ee8be3cbcdca8e
neil <neil.r.simmons@gmail.com>
2016-04-05 00:28:05 -0500

    Changed the Main GUI a lot. Added some assets for navigation.

--- app/src/main/java/com/example/ameerak/calendarproject_team4/CalendarMain2.java
+++ app/src/main/java/com/example/ameerak/calendarproject_team4/CalendarMain2.java
@@ -1,7 +1,13 @@
 package com.example.ameerak.calendarproject_team4;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CalendarMain2 extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class CalendarMain2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_main2);
+        CalendarView cView = new CalendarView(this);
+
     }
 }
+
+
+
