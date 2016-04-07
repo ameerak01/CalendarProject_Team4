df62099d3093e044385b3a68eb01a5003f50b5c8
neil <neil.r.simmons@gmail.com>
2016-04-07 11:46:00 -0500

    Fixed some issues where the default calendar parameters (Day, month, year) where being used for calculations rather than the calendar the function was trying to operate on.

--- app/src/main/java/com/example/ameerak/calendarproject_team4/TextAdapter.java
+++ app/src/main/java/com/example/ameerak/calendarproject_team4/TextAdapter.java
@@ -99,27 +99,27 @@ public class TextAdapter extends BaseAdapter {
 
             // Check that the previous month was not december of the previous year
             if (GregorianCalendar.MONTH - 1 >= 0) {
-                return new GregorianCalendar(GregorianCalendar.YEAR, GregorianCalendar.MONTH - 1, today);
+                return new GregorianCalendar(calendar.get(GregorianCalendar.YEAR), calendar.get(GregorianCalendar.MONTH) - 1, today);
             }
             else {
-                return new GregorianCalendar(GregorianCalendar.YEAR - 1, 12, today);
+                return new GregorianCalendar(calendar.get(GregorianCalendar.YEAR) - 1, 12, today);
             }
 
         }
         // Indicates the current position of the grid is the current month
         else if (workingPosition <= (daysInMonth + firstDayOfMonth)) {
             today = workingPosition - firstDayOfMonth;
-            return new GregorianCalendar(GregorianCalendar.YEAR, GregorianCalendar.MONTH, today);
+            return new GregorianCalendar(calendar.get(GregorianCalendar.YEAR), calendar.get(GregorianCalendar.MONTH), today);
         }
         // Otherwise the current position is outside of current month.
         else {
             today = workingPosition - firstDayOfMonth - daysInMonth;
             // Check that the next month is not January of the next year
-            if(GregorianCalendar.MONTH + 1 < 12) {
-                return new GregorianCalendar(GregorianCalendar.YEAR, GregorianCalendar.MONTH + 1, today);
+            if(calendar.get(GregorianCalendar.MONTH) + 1 < 12) {
+                return new GregorianCalendar(calendar.get(GregorianCalendar.YEAR), calendar.get(GregorianCalendar.MONTH) + 1, today);
             }
             else {
-                return new GregorianCalendar(GregorianCalendar.YEAR + 1, 1, today);
+                return new GregorianCalendar(calendar.get(GregorianCalendar.YEAR) + 1, 1, today);
             }
         }
     }
