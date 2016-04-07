49126c847a33e5269501ce628826d4004a2fcf62
ameerak01 <ameera.khan@mavs.uta.edu>
2016-04-07 00:01:39 -0500

    changed pieces on the ShareEvent

--- app/src/main/java/com/example/ameerak/calendarproject_team4/ShareEvent.java
+++ app/src/main/java/com/example/ameerak/calendarproject_team4/ShareEvent.java
@@ -1,7 +1,11 @@
 package com.example.ameerak.calendarproject_team4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShareEvent extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class ShareEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_event);
+
+        }
     }
-}
+
