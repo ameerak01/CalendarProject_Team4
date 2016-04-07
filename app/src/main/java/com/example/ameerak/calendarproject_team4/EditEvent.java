c04676c16ffa23755fe6973f99d7f347d29117ff
Sam <samdieng@hotmail.com>
2016-04-07 18:37:52 -0500

    edit event

--- app/src/main/java/com/example/ameerak/calendarproject_team4/EditEvent.java
+++ app/src/main/java/com/example/ameerak/calendarproject_team4/EditEvent.java
@@ -43,5 +43,7 @@ public class EditEvent extends AppCompatActivity
         eventDescription = (EditText) findViewById(R.id.description_editText);
         eventDescription.setText(event.getDescription());
 
+
+
     }
 }
